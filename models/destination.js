b404bb40f1d912988c865d201f97ca5639ecba9e
aroseca15 <69179642+aroseca15@users.noreply.github.com>
2021-01-23 15:15:09 -0700

    adjustments for team hard coding some sql tables.

--- models/destination.js
+++ models/destination.js
@@ -1,63 +1,63 @@
 module.exports = function (sequelize, DataTypes) {
-    const Destinations = sequelize.define('Destinations', {
-        id: { 
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true,
-            type: DataTypes.INTEGER
-        },
+    // const Destinations = sequelize.define('Destinations', {
+    //     id: { 
+    //         allowNull: false,
+    //         autoIncrement: true,
+    //         primaryKey: true,
+    //         type: DataTypes.INTEGER
+    //     },
 
-        city: {
-            type: DataTypes.STRING, 
-            allowNull: false,
+    //     city: {
+    //         type: DataTypes.STRING, 
+    //         allowNull: false,
             
-        },
+    //     },
 
-        country: {
-            type: DataTypes.STRING, 
-            allowNull: false,
-        },
+    //     country: {
+    //         type: DataTypes.STRING, 
+    //         allowNull: false,
+    //     },
 
-        latitude: {
-            type: DataTypes.DECIMAL, 
-            allowNull: false,
-            validate: {
-                len: [1, 60]
-            }
-        },
+    //     latitude: {
+    //         type: DataTypes.DECIMAL, 
+    //         allowNull: false,
+    //         validate: {
+    //             len: [1, 60]
+    //         }
+    //     },
 
-        longitude: {
-            type: DataTypes.DECIMAL, 
-            allowNull: false,
-            validate: {
-                len: [1, 60]
-            }
-        },
+    //     longitude: {
+    //         type: DataTypes.DECIMAL, 
+    //         allowNull: false,
+    //         validate: {
+    //             len: [1, 60]
+    //         }
+    //     },
 
-        languageUsed: {
-            type: DataTypes.STRING, 
-            allowNull: false,
-        },
+    //     languageUsed: {
+    //         type: DataTypes.STRING, 
+    //         allowNull: false,
+    //     },
 
-        currency: {
-            type: DataTypes.TEXT, 
-            allowNull: false
-        },
+    //     currency: {
+    //         type: DataTypes.TEXT, 
+    //         allowNull: false
+    //     },
 
-        currencySymbol: {
-            type: DataTypes.TEXT, 
-            allowNull: false
-        },
+    //     currencySymbol: {
+    //         type: DataTypes.TEXT, 
+    //         allowNull: false
+    //     },
 
 
 
-        body: {
-            type: DataTypes.TEXT,
-            allowNull: true,
-        }
-    });
+    //     body: {
+    //         type: DataTypes.TEXT,
+    //         allowNull: true,
+    //     }
+    // });
 
     
 
-    return Destinations;
+    // return Destinations;
 };
